c8d249aaffd63e7c6f40b12420bd61edd3cfbc06
Vijaymahadevan97 <vijaymahadevan97@gmail.com>
2024-03-07 23:03:38 +0530

    resolved userSchema problem

--- lib/database/models/user.model.ts
+++ lib/database/models/user.model.ts
@@ -18,7 +18,7 @@ const UserSchema= new Schema({
     clerkId:{type:String, required:true,unique:true},
     email:{type:String, required:true,unique:true},
     userName:{type:String, required:true,unique:true},
-    photo:{type:URL,required:true},
+    photo:{type:String,required:true},
     firstName:{type:String},
     lastName:{type:String},
     planId:{type:Number, default:1},
